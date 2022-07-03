2dc783fcd30f5a40a9f98659184471cfe03cf7d3
Narendra2github <bnarendra052@gmail.com>
2022-07-03 23:06:05 +0530

    ********** ****  **** ***    *** **      ** *        *
    above pattern Commit

--- ScaleUp/src/com/scale/Pattern.java
+++ ScaleUp/src/com/scale/Pattern.java
@@ -1,7 +1,28 @@
 package com.scale;
 
+import java.util.Scanner;
+
 public class Pattern {
 public static void main(String[] args) {
-	
+	Scanner sc=new Scanner(System.in);
+	int n=5;//sc.nextInt();
+	int j=0;
+	for(int i=0;i<n;i++) {
+		for( j=1;j<=n-i;j++) {
+			System.out.print("*");
+		}
+		for(j=0;j<i;j++) {
+		 System.out.print(" ");
+		}
+		for(j=n;j<2*n;j++) {
+			if(j<n+i) {
+				System.out.print(" ");
+			}else {
+			System.out.print("*");
+			}
+		}
+		System.out.println();
+		
+	}
 }
 }
